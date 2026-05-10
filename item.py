56a58307b83f9446411cb46068cdd6e230581331
unknown <ivanderdaniel70@gmail.com>
2026-05-10 19:43:33 +0700

    class penulis ivan

--- item.py
+++ item.py
@@ -16,3 +16,11 @@ class ItemPerpustakaan(ABC):
     
     def getJudul(self,judul):
        return self.__judul
+    
+class Penulis:
+    def __init__(self,nama,kewarganegaraan):
+        self.nama = nama
+        self.kewarganegaraan = kewarganegaraan
+    
+    def __str__(self):
+        return f'{self.nama} ({self.kewarganegaraan})'
