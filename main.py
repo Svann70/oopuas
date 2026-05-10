57646d178b7015dbfe800ff01b2162c9e9ea8f06
maulana-99 <maulana.be.dv@gmail.com>
2026-05-10 20:26:35 +0700

    main ilham

---
+++ main.py
@@ -0,0 +1,48 @@
+from manajer import ManajerPerpustakaan
+
+def main():
+    manager = ManajerPerpustakaan()
+
+    while True:
+        print("\n=== MENU ===")
+        print("1. Tampilkan Semua Koleksi")
+        print("2. Tambah Buku Baru")
+        print("3. Urutkan Koleksi")
+        print("4. Cari Buku")
+        print("5. Keluar")
+
+        pilihan = input("Pilih menu: ")
+
+        if pilihan == "1":
+            manager.tampilkan_koleksi()
+
+        elif pilihan == "2":
+            manager.tambah_buku()
+
+        elif pilihan == "3":
+            manager.urutkan_koleksi()
+
+        elif pilihan == "4":
+            try:
+                id_cari = int(input("Masukkan ID Buku: "))
+                hasil = manager.cari_buku(id_cari)
+
+                if hasil:
+                    print("Ditemukan:")
+                    print(hasil.deskripsi())
+                else:
+                    print("Buku tidak ditemukan.")
+
+            except ValueError:
+                print("ID harus angka.")
+
+        elif pilihan == "5":
+            print("Program selesai.")
+            break
+
+        else:
+            print("Menu tidak valid.")
+
+
+if __name__ == "__main__":
+    main()
